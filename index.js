4628d80ea859769d497293ccb189dc6c69e16357
Micah Teeuws <tutmirage@gmail.com>
2022-12-03 20:54:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+class BufferedFile {
+
+    #source;
+
+    constructor(source) {
+        this.#source = source;
+    }
+
+    name() {
+        return this.#source.name;
+    }
+
+    size() {
+        return this.#source.size;
+    }
+
+    async arrayBuffer(start, end) {
+        let blob = this.#source.slice(start, end);
+        return await blob.arrayBuffer();
+    }
+
+}
