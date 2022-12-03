10eb3125e83866c67aa9de9f07fafa6cbefaf820
Micah Teeuws <tutmirage@gmail.com>
2022-12-03 21:17:43 +0300

    Create BufferedFile.js

---
+++ BufferedFile.js
@@ -0,0 +1,22 @@
+export default class BufferedFile {
+
+    #source;
+
+    constructor(source) {
+        this.#source = source;
+    }
+
+    name() {
+        return this.#source.name;
+    }
+
+    size() {
+        return this.#source.size;
+    }
+
+    async arrayBuffer(start, end) {
+        let blob = this.#source.slice(start, end);
+        return await blob.arrayBuffer();
+    }
+
+}
