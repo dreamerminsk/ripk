df1a40d56649fbf5fe3e9cc208c4d381da7a8754
Micah Teeuws <tutmirage@gmail.com>
2022-12-06 17:53:18 +0300

    Create SimpleGreeting.ts

---
+++ SimpleGreeting.ts
@@ -0,0 +1,5 @@
+import {LitElement, css, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('simple-greeting')
+export class SimpleGreeting extends LitElement {}
