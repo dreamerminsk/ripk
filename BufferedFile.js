b48d534e79f8746460a4b6c88e5a773149eb8e4e
Micah Teeuws <tutmirage@gmail.com>
2025-03-28 00:52:46 +0300

    Update BufferedFile.js

--- BufferedFile.js
+++ BufferedFile.js
@@ -2,6 +2,7 @@ export default class BufferedFile {
 
     constructor(source) {
         this.source = source;
+        this.buffers = new Map();
     }
 
     name() {
@@ -18,7 +19,11 @@ export default class BufferedFile {
 
     async getBuffer(byteOffset) {
         let idx = byteOffset >> 13;
-        return arrayBuffer(idx << 13, (idx + 1) << 13);
+        if (this.buffers.has(idx)) {
+            return this.buffers.get(idx);
+        } else {
+            return arrayBuffer(idx << 13, (idx + 1) << 13);
+        }
     }
 
     async arrayBuffer(start, end) {
