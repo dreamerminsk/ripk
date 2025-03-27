58ae9e9bc39985f1f8b7a885ed0a2f67ff914ab7
Micah Teeuws <tutmirage@gmail.com>
2025-03-27 18:52:48 +0300

    Update BufferedFile.js

--- BufferedFile.js
+++ BufferedFile.js
@@ -1,21 +1,25 @@
 export default class BufferedFile {
 
-    #source;
+    source;
 
     constructor(source) {
-        this.#source = source;
+        this.source = source;
     }
 
     name() {
-        return this.#source.name;
+        return this.source.name;
     }
 
     size() {
-        return this.#source.size;
+        return this.source.size;
+    }
+
+    getUint8(byteOffset) {
+        const buffer = getBuffer(byteOffset);
     }
 
     async arrayBuffer(start, end) {
-        let blob = this.#source.slice(start, end);
+        let blob = this.source.slice(start, end);
         return await blob.arrayBuffer();
     }
 
