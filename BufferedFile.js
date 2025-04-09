92c26fea7d090c600cd234e8d5b9dffb35d2bb08
Micah Teeuws <tutmirage@gmail.com>
2025-04-09 18:09:08 +0300

    Update BufferedFile.js

--- BufferedFile.js
+++ BufferedFile.js
@@ -1,5 +1,4 @@
 export default class BufferedFile {
-
     constructor(source) {
         this.source = source;
         this.buffers = new Map();
@@ -14,21 +13,26 @@ export default class BufferedFile {
     }
 
     async getUint8(byteOffset) {
-        const buffer = this.getBuffer(byteOffset);
+        const buffer = await this.getBuffer(byteOffset);
+        const localOffset = byteOffset & 0x1FFF;
+        return buffer[localOffset];
     }
 
     async getBuffer(byteOffset) {
-        let idx = byteOffset >> 13;
+        const idx = byteOffset >> 13;
         if (this.buffers.has(idx)) {
             return this.buffers.get(idx);
         } else {
-            return arrayBuffer(idx << 13, (idx + 1) << 13);
+            const start = idx << 13;
+            const end = Math.min((idx + 1) << 13, this.size());
+            const buffer = await this.arrayBuffer(start, end);
+            this.buffers.set(idx, new Uint8Array(buffer));
+            return this.buffers.get(idx);
         }
     }
 
     async arrayBuffer(start, end) {
-        let blob = this.source.slice(start, end);
+        const blob = this.source.slice(start, end);
         return await blob.arrayBuffer();
     }
-
 }
