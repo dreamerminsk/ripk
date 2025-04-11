db6be3c4fd98f581598cc8919741927593f99079
Micah Teeuws <tutmirage@gmail.com>
2025-04-11 15:10:14 +0300

    Create src/lrucache.js

---
+++ src/lrucache.js
@@ -0,0 +1,29 @@
+export default class LRUCache {
+    constructor(limit = 8) {
+        this.limit = limit;
+        this.cache = new Map();
+    }
+
+    get(key) {
+        if (!this.cache.has(key)) return null;
+
+        const value = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, value);
+        return value;
+    }
+
+    set(key, value) {
+        if (this.cache.has(key)) {
+            this.cache.delete(key);
+        } else if (this.cache.size >= this.limit) {
+            const firstKey = this.cache.keys().next().value;
+            this.cache.delete(firstKey);
+        }
+        this.cache.set(key, value);
+    }
+
+    has(key) {
+        return this.cache.has(key);
+    }
+}
